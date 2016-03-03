9e4067a672077014db61821c9888fd36de620a8a
ngpatel143 <ngpatel@outlook.com>
2016-03-03 23:03:05 +0100

    redis added

--- complete_install.sh
+++ complete_install.sh
@@ -71,3 +71,15 @@ service mongod start
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 
+# redis install
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+sudo make install
+cd utils
+sudo ./install_server.sh
+
+sudo service redis_6379 start
+sudo service redis_6379 stop
